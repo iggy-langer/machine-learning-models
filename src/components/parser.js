0756d3c91dbc79db5a9da313edca5d2656c4bca9
iggy-langer <262610318+iggy-langer@users.noreply.github.com>
2026-03-17 13:24:32 +1000

    Added CSV parsing functionality to parser module

---
+++ src/components/parser.js
@@ -0,0 +1,21 @@
+function parseCsv(filePath) {
+  return new Promise((resolve, reject) => {
+    const fs = require('fs');
+    const csv = require('csv-parser');
+    const data = [];
+
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on('data', (row) => {
+        data.push(row);
+      })
+      .on('end', () => {
+        resolve(data);
+      })
+      .on('error', (error) => {
+        reject(error);
+      });
+  });
+}
+
+module.exports = parseCsv;
